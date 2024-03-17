65058395431a41f825440dd9175579537b016c4a
MarJose123 <18107626+MarJose123@users.noreply.github.com>
2024-03-17 16:36:30 +0800

    Add device route
    Signed-off-by: MarJose123 <18107626+MarJose123@users.noreply.github.com>

--- routes/api.php
+++ routes/api.php
@@ -1,1 +1,11 @@
 <?php
+
+use MarJose123\SmsGateway\Http\Controllers\DeviceController;
+
+Route::prefix('sms-gateway')
+    ->middleware(['auth:api','auth:web'])
+    ->group(function () {
+        Route::apiResources([
+            'devices' => DeviceController::class,
+        ]);
+});
