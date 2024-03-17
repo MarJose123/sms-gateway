c4746426ce6b9e93fd5b21f4dfde0b1ad30bc601
MarJose123 <18107626+MarJose123@users.noreply.github.com>
2024-03-17 16:36:18 +0800

    Add interface for User Model
    Signed-off-by: MarJose123 <18107626+MarJose123@users.noreply.github.com>

--- Contracts/SmsPorter.php
+++ Contracts/SmsPorter.php
@@ -3,4 +3,10 @@
 interface SmsPorter
 {
     public function canSendSms(): bool{}
+
+    /**
+     * @return bool
+     * @description if true, then all the data will be visible to the current logged-in user
+     */
+    public function canSeeAll(): bool{}
 }
