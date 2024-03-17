7a7ac66c3ecee5e2f6b720049503b7ae51e48c89
MarJose123 <18107626+MarJose123@users.noreply.github.com>
2024-03-17 16:37:51 +0800

    Add traits for User Model
    Signed-off-by: MarJose123 <18107626+MarJose123@users.noreply.github.com>

---
+++ src/Facades/Contracts/Concern/HasDevice.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace MarJose123\SmsGateway\Facades\Contracts\Concern;
+
+use MarJose123\SmsGateway\Models\Devices;
+
+trait HasDevice
+{
+    public function devices()
+    {
+        return $this->hasMany(Devices::class, 'user');
+    }
+
+    public function canSendSms(): bool{
+        return true;
+    }
+
+    /**
+     * @return bool
+     * @description if true, then all the data will be visible to the current logged-in user
+     */
+    public function canSeeAll(): bool{
+        return true;
+    }
+
+}
