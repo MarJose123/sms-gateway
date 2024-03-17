4faaf3876d4be18b80b138a6b67fe4305be97966
MarJose123 <18107626+MarJose123@users.noreply.github.com>
2024-03-17 15:05:18 +0800

    add migration location
    Signed-off-by: MarJose123 <18107626+MarJose123@users.noreply.github.com>

--- src/SmsGatewayServiceProvider.php
+++ src/SmsGatewayServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace MarJose123\SmsGateway;
 
+use Illuminate\Support\Facades\File;
+use Spatie\LaravelPackageTools\Commands\InstallCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 use MarJose123\SmsGateway\Commands\SmsGatewayCommand;
@@ -19,8 +21,22 @@ class SmsGatewayServiceProvider extends PackageServiceProvider
             ->name('sms-gateway')
             ->hasConfigFile()
             ->hasViews()
-            ->hasMigrations([])
-            ->runsMigrations()
+            ->hasMigrations($this->migrationFiles())
+            ->hasInstallCommand(function (InstallCommand $command) {
+                $command
+                    ->askToRunMigrations()
+                    ->askToStarRepoOnGitHub('marjo123/sms-gateway');
+            })
             ;
+    }
+
+    private function migrationFiles(): array
+    {
+        $migrationFiles = collect();
+        foreach (File::files('../database/migrations') as $file) {
+            $_file = pathinfo($file);
+            $migrationFiles->push($_file['filename']);
+        }
+        return  $migrationFiles->flatten()->toArray();
     }
 }
