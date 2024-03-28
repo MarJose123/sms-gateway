7c49017c6d32bc02e19b7ab1ef6e295c1fdfba63
MarJose123 <18107626+MarJose123@users.noreply.github.com>
2024-03-28 21:53:27 +0800

    feat: move routes to v1 prefix
    This commit moves the routes in the `routes/api.php` file to a `/v1` prefix.
    
    This change was made to improve the organization of the routes and to make it easier to add new routes in the future.
    
    Signed-off-by: MarJose123 <18107626+MarJose123@users.noreply.github.com>

--- routes/api.php
+++ routes/api.php
@@ -3,10 +3,10 @@
 use MarJose123\SmsGateway\Http\Controllers\DeviceController;
 use MarJose123\SmsGateway\Http\Controllers\MessageController;
 
-Route::prefix('sms-gateway')
+Route::prefix('v1')
     ->middleware(['auth:sanctum'])
     ->group(function () {
-        Route::prefix('v1')->group(function () {
+        Route::prefix('sms-gateway')->group(function () {
             Route::patch('/device/{id}', [DeviceController::class, 'patch']);
             Route::apiResource('devices', DeviceController::class);
             Route::patch('/message/{id}', [MessageController::class, 'patch']);
