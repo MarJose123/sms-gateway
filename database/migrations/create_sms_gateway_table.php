d240de7143ca2c9c2d8101ad61124b1fba764460
MarJose123 <MarJose123@users.noreply.github.com>
2024-03-29 04:17:40 +0000

    Fix styling

--- database/migrations/create_sms_gateway_table.php
+++ database/migrations/create_sms_gateway_table.php
@@ -4,7 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     public function up(): void
     {
         Schema::create('contacts', static function (Blueprint $table) {
@@ -40,7 +41,6 @@ return new class extends Migration {
             $table->timestamps();
         });
 
-
         Schema::create('messages', static function (Blueprint $table) {
             $table->uuid('id')->primary();
             $table->foreignId('user');
@@ -53,7 +53,6 @@ return new class extends Migration {
             $table->softDeletes();
             $table->timestamps();
         });
-
 
     }
 
