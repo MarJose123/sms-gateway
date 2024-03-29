afe81c77827dd9e785e2f654770f7c8da50901c4
MarJose123 <18107626+MarJose123@users.noreply.github.com>
2024-03-29 11:57:06 +0800

    feat(api): update api routes
    This commit updates the API routes to use the `sms-gateway` prefix.
    
    The following changes were made:
    
    - The `v1` prefix was removed from the API routes.
    - The `sms-gateway` prefix was added to the API routes.
    - The `api/v1` prefix was added to the API routes.
    
    Signed-off-by: MarJose123 <18107626+MarJose123@users.noreply.github.com>

--- routes/api.php
+++ routes/api.php
@@ -4,10 +4,10 @@ use MarJose123\SmsGateway\Http\Controllers\DeviceController;
 use MarJose123\SmsGateway\Http\Controllers\MessageController;
 use MarJose123\SmsGateway\Http\Controllers\MessageFailedController;
 
-Route::prefix('v1')
+Route::prefix('sms-gateway')
     ->middleware(['auth:sanctum'])
     ->group(function () {
-        Route::prefix('sms-gateway')->group(function () {
+        Route::prefix('api/v1')->group(function () {
             Route::patch('/device/{id}', [DeviceController::class, 'patch']);
             Route::apiResource('devices', DeviceController::class);
             Route::patch('/message/{id}', [MessageController::class, 'patch']);
